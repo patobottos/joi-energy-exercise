693b341b6d9ac9f84debb608d1903de311ca037b
Pato <patobottos@gmail.com>
2024-10-08 16:40:30 +0200

    ⚡️ Adding functionality to the 4 buttons

--- components/EnergyConsumption.jsx
+++ components/EnergyConsumption.jsx
@@ -1,21 +1,44 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { renderChart } from "../utils/chart.js";
 import { groupByDay, sortByTime } from "../utils/reading";
 import Button from "./Button.jsx";
 
 export const EnergyConsumption = ({ readings }) => {
+  const [filteredReadings, setFilteredReadings] = useState([]);
   const containerId = "usageChart";
+
   useEffect(() => {
-    renderChart(containerId, sortByTime(groupByDay(readings)).slice(-30));
-  }, []);
+    setFilteredReadings(sortByTime(groupByDay(readings)).slice(-30));
+  }, [readings]);
+
+  useEffect(() => {
+    renderChart(containerId, filteredReadings);
+  }, [filteredReadings]);
+
+  const show30DaysConsumption = () => {
+    setFilteredReadings(sortByTime(groupByDay(readings)).slice(-30));
+  };
+
+  const show7DaysConsumption = () => {
+    setFilteredReadings(sortByTime(groupByDay(readings)).slice(-7));
+  };
+
+  const showDailyConsumption = () => {
+    setFilteredReadings(groupByDay(readings));
+  };
+
+  const showHourlyConsumption = () => {
+    setFilteredReadings(sortByTime(readings).slice(-24));
+  };
 
   return (
     <>
       <h1 className="regular darkgray line-height-1 mb3">Energy consumption</h1>
-      <section className="mb3">
-        <Button>One</Button>
-        <Button>Two</Button>
-        <Button>Three</Button>
+      <section className="mb3 flex">
+        <Button onClick={show30DaysConsumption}>Last 30 Days</Button>
+        <Button onClick={show7DaysConsumption}>Last 7 Days</Button>
+        <Button onClick={showDailyConsumption}>Dayly Consumption</Button>
+        <Button onClick={showHourlyConsumption}>Hourly Consumption</Button>
       </section>
       <section className="chartHeight mb3">
         <canvas id={containerId} />
